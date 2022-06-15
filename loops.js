4e4c1776fdafd751164a2afd626e6f2663db7ded
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-15 16:03:50 +0800

    xs

---
+++ loops.js
@@ -0,0 +1,119 @@
+alert('loops tutorial');
+
+for (let u = 0; u< 5; u++)
+{
+	console.log(u);
+}
+
+
+const names = ['a','b','c'];
+
+for (let i = 0; i<names.length; i++)
+{
+	console.log(names[i]);
+	let html = `<div>${names[i]}</div>`;
+	console.log(html);
+}
+
+let uu = 0;
+console.log('in while loop');
+while (uu < names.length)
+{
+	console.log(names[uu]);
+	uu++;
+}
+
+
+uu = 0;
+console.log('startuing do while');
+do {
+	
+	console.log(names[uu]);
+	uu++;
+}while (uu < names.length)
+
+console.log('starting if else');
+
+
+const password = 'pass';
+
+if (password.length>= 8)
+{
+	console.log('that password is proper');
+}else
+{
+	console.log('that password is too short');
+}
+
+const age = 9;
+
+if (age >= 15)
+{
+	console.log('you are abit old');
+}else if (age >= 10)
+{
+	console.log('you are not old');
+}else
+{
+	console.log('you are young');
+}
+
+
+
+if ((age == 9) && (true))
+{
+	console.log('we\'re in here');
+}
+
+if ((age ==10) || true)
+{
+	console.log('in here ageain');
+}
+
+
+let answer = false;
+
+if (!answer)
+{
+	console.log('still stuck in purgatory');
+}
+
+
+for (let i = 0; i< 10; i++)
+{
+	console.log(i);
+	if (i == 6)
+	{
+		break;
+	}
+}
+
+for (let i = 0; i< 10; i++)
+{
+	
+	if (i == 6)
+	{
+		continue;
+	}
+	console.log(i);
+}
+
+
+console.log('starting scope');
+
+
+let scopeVar = 22;
+
+if (true)
+{
+	let scopeVar = 26;
+	let name123 = 'shaun';
+	console.log(scopeVar);
+	console.log(name123);
+	var omg = 'omg';
+}
+
+
+console.log(omg);
+console.log(scopeVar);
+//console.log(name123);
