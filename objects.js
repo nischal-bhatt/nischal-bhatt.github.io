6b186ded762fb83b6759a456133b7dfd22dc6332
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-15 21:33:48 +0800

    sad

---
+++ objects.js
@@ -0,0 +1,55 @@
+alert('hello objects');
+
+let user = {
+	name: 'crystal',
+	age: 30,
+	email: 'crystal@thenetninja.co.uk',
+	blogs: ['photos', 'kudos'],
+
+	login: function() {
+		console.log('user logged in ');
+	},
+
+	logout: function() {
+		console.log('user logged out');
+	},
+	logBlogs: function() {
+		for (let aub = 0; aub < this.blogs.length; aub++) {
+			console.log(this.blogs[aub]);
+		}
+	}
+};
+
+console.log(user);
+console.log(user.name);
+
+user.age = 25;
+
+console.log(user.age);
+
+console.log(user['age']);
+
+console.log(typeof user);
+
+user.login();
+
+const name1 = 'hey';
+console.log(name1.toUpperCase());
+
+user.logout();
+user.logBlogs();
+
+
+let arrayOfObjects = [
+	{
+		title: 'billy goat'
+	},
+	{
+		jumanji: 'hi'
+	},
+	{
+		jurrasic_park: 'heya'
+	}
+];
+
+console.log(arrayOfObjects);
