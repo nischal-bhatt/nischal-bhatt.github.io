c1ae3d0173056c8b2d1d4d7344a94c31e2645c16
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-15 22:12:31 +0800

    kjas

--- domjs.js
+++ domjs.js
@@ -1,1 +1,21 @@
-alert('hi dom');
+alert(document.location);
+
+const para = document.querySelector('p');
+
+// first p tag is grabbed, the rest are ignored
+
+console.log(para);
+
+const para2 = document.querySelector('.error');
+
+console.log(para2);
+
+const para3 = document.querySelector('div.error');
+
+console.log(para3);
+
+//para3.innerHTML = 'changing';
+
+const paras = document.querySelectorAll('p');
+
+console.log(paras[2]);
