d34953d2bba6730d8a9cc02c62c37f29da37cb25
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-16 13:37:19 +0800

    asdads

---
+++ moredomeventsjs.js
@@ -0,0 +1,18 @@
+alert('hi dom events');
+
+const copy = document.querySelector('.copy-me');
+
+copy.addEventListener('copy', () => {
+	console.log('copying');
+});
+
+const box = document.querySelector('.box');
+
+box.addEventListener('mousemove', (e) => {
+	 //console.log(e.offsetX, e.offsetY);
+     box.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY}`;
+});
+
+document.addEventListener('wheel' ,e => {
+	 console.log(e.pageX, e.pageY);
+});
