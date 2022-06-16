a10fff8ee671b406ea9f8a040c07240a3d997fa7
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-16 11:34:05 +0800

    kj

--- parentchildrenjs.js
+++ parentchildrenjs.js
@@ -19,3 +19,23 @@ console.log(titl.parentElement);
 console.log(titl.parentElement.parentElement);
 console.log(titl.nextElementSibling);
 console.log(titl.nextElementSibling.parentElement.children);
+
+
+//const butt = document.querySelector('button');
+
+//butt.addEventListener('click', ()=> {
+//	console.log('you clicked me');
+//});
+
+const itemz = document.querySelectorAll('li');
+
+itemz.forEach(item => {
+	item.addEventListener('click', e => {
+		//console.log('item clicked');
+	    console.log(e.target);
+	    e.target.style.textDecoration = 'line-through';
+	
+	});
+});
+
+	
